15a5e1cf7922fa06e3c127b56517cf8a5cd4dd86
Matt Kuras <mattkuras@gmail.com>
2020-03-25 00:45:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    students_array.each do |student|
+      student.each do |name, loco|
+        
     binding.pry 
   end
 
