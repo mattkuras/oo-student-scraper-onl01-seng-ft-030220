09f856d9df59d7fef4b2ba5d68c27a80073055cf
Matt Kuras <mattkuras@gmail.com>
2020-03-25 00:46:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,9 +12,10 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student.each do |name, loco|
+      
         
     binding.pry 
+  end 
   end
 
   def add_student_attributes(attributes_hash)
